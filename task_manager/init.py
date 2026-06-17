fbeb620c8f897d9b702c438ea456bcbd2daaffa4
ruriga002 <eruriga002@gmail.com>
2026-06-17 11:28:20 +0300

    changes made

---
+++ task_manager/init.py
@@ -0,0 +1,12 @@
+from .task_utils import (
+    add_task,
+    mark_task_as_complete,
+    view_pending_tasks,
+    calculate_progress
+)
+
+from .validation import (
+    validate_task_title,
+    validate_task_description,
+    validate_due_date
+)
